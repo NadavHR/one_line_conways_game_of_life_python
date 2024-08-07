9c62b7c5c52c3b718f9e934d80d54880ffa2ac70
senpairo111 <nadavh2611@gmail.com>
2024-08-08 00:41:53 +0300

    improved it to 2 lines (3 if you want quality of life)
    uses recursion for infinite loop which is a bad idea as it reaches a recursion limit, its is possible to make a 3 lines version with a 'while true' instead of recursion for it to not hit a recursion limit but it will come at the cost of clearing and waiting on generations

--- main.py
+++
@@ -1,4 +0,0 @@
-field = ([[1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1]] * 10) + ([[0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1]] * 10)
-while True:
-    print("\n" + "\n".join(["".join('#' if j == 1 else ' ' for j in i) for i in field]))
-    field = [[0 if (sum([field[(y + i - 1) % 20][(x + j - 1) % 20] for j in range(3) for i in range(3)]) - field[y][x]) <= 1 or (sum([field[(y + i - 1) % 20][(x + j - 1) % 20] for j in range(3) for i in range(3)]) - field[y][x]) >= 4 else 1 for x in range(len(field[y]))] for y in range(len(field))]
---
+++ three_lines.py
@@ -0,0 +1,26 @@
+import os, time
+# memory clear clears stdout
+# memory init initializes field
+# memory update updates the field
+# memory field is [field]
+# memory print prints the field
+# memory run_generations starts running generations
+# memory start starts the game
+memory = {'clear': lambda: (os.system("cls")),
+          'init': lambda mem: mem['field'].append(*(
+                ([([[1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1]] * 10) +
+                ([[0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1]] * 10)]))),
+          'update': lambda mem: (field := mem['field'][0],
+                                 new_field := [[0 if (sum([field[(y + i - 1) % 20][(x + j - 1) % 20]
+                                    for j in range(3) for i in range(3)]) - field[y][x]) <= 1
+                                    or (sum([field[(y + i - 1) % 20][(x + j - 1) % 20]
+                                    for j in range(3) for i in range(3)]) - field[y][x]) >= 4
+                                    else 1 for x in range(len(field[y]))] for y in range(len(field))],
+                                 mem['field'].clear(),
+                                 mem['field'].append(new_field)),
+          'print': lambda mem: (field := mem['field'][0],
+                                print("\n".join(["".join('#' if j == 1 else ' ' for j in i) for i in field]))),
+          'run_generations': lambda mem: (mem['clear'](), mem['print'](mem), time.sleep(1), mem['update'](mem), mem['run_generations'](mem)),
+          'start': lambda mem: (mem['init'](mem), mem['run_generations'](mem)),
+          'field': []}
+memory['start'](memory)
