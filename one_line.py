55f41072d0afc3fff90478d7876a5706e768bce3
senpairo111 <nadavh2611@gmail.com>
2024-08-08 16:21:13 +0300

    no more recursion limit

--- one_line.py
+++ one_line.py
@@ -3,8 +3,8 @@
 # memory update updates the field
 # memory field is [field]
 # memory print prints the field
-# memory run_generations starts running generations
-# memory start starts the game
+# memory run_generation runs a generation
+# # memory start starts the game
 (lambda: (memory := {'clear': lambda: (__import__('os').system("cls")),
           'init': lambda mem: mem['field'].append(*(
                 ([([[1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1]] * 10) +
@@ -19,7 +19,8 @@
                                  mem['field'].append(new_field)),
           'print': lambda mem: (field := mem['field'][0],
                                 print("\n".join(["".join('#' if j == 1 else ' ' for j in i) for i in field]))),
-          'run_generations': lambda mem: (mem['clear'](), mem['print'](mem), __import__('time').sleep(1), mem['update'](mem), mem['run_generations'](mem)),
-          'start': lambda mem: (mem['init'](mem), mem['run_generations'](mem)),
+          'run_generation': lambda mem: (mem['clear'](), mem['print'](mem), __import__('time').sleep(0.5), mem['update'](mem)),
+          'start': lambda mem: (mem['init'](mem), __import__('functools').reduce(lambda a, b: mem['run_generation'](mem), iter(int, 1))),
           'field': []}, memory['start'](memory)))()
+
 
